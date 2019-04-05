6a6dba7459abab65c9841579c52671529ab7d7b4
Spencer Arnold <sa.variable@gmail.com>
2019-04-05 12:40:42 -0500

    Init

---
+++ FilterExcelDuplicates/FilterExcelDuplicates/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace FilterExcelDuplicates
+{
+    partial class ExcelDuplicateSifter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExcelDuplicateSifter));
+            this.Browse1 = new System.Windows.Forms.Button();
+            this.BrowseTB1 = new System.Windows.Forms.TextBox();
+            this.BrowseLBL1 = new System.Windows.Forms.Label();
+            this.BrowseLBL2 = new System.Windows.Forms.Label();
+            this.SiftButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SheetNameTB = new System.Windows.Forms.TextBox();
+            this.SheetNameLBL = new System.Windows.Forms.Label();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.hasHeaderRow = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Browse1
+            // 
+            this.Browse1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Browse1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Browse1.Location = new System.Drawing.Point(174, 71);
+            this.Browse1.Name = "Browse1";
+            this.Browse1.Size = new System.Drawing.Size(87, 24);
+            this.Browse1.TabIndex = 0;
+            this.Browse1.Text = "Browse...";
+            this.Browse1.UseVisualStyleBackColor = false;
+            this.Browse1.Click += new System.EventHandler(this.Browse1_Click);
+            // 
+            // BrowseTB1
+            // 
+            this.BrowseTB1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseTB1.Location = new System.Drawing.Point(35, 101);
+            this.BrowseTB1.Name = "BrowseTB1";
+            this.BrowseTB1.Size = new System.Drawing.Size(351, 23);
+            this.BrowseTB1.TabIndex = 2;
+            this.BrowseTB1.TextChanged += new System.EventHandler(this.BrowseTB1_TextChanged);
+            // 
+            // BrowseLBL1
+            // 
+            this.BrowseLBL1.AutoSize = true;
+            this.BrowseLBL1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrowseLBL1.Location = new System.Drawing.Point(121, 39);
+            this.BrowseLBL1.Name = "BrowseLBL1";
+            this.BrowseLBL1.Size = new System.Drawing.Size(198, 15);
+            this.BrowseLBL1.TabIndex = 4;
+            this.BrowseLBL1.Text = "Select the Excel file you want to sift";
+            // 
+            // BrowseLBL2
+            // 
+            this.BrowseLBL2.AutoSize = true;
+            this.BrowseLBL2.Location = new System.Drawing.Point(348, 196);
+            this.BrowseLBL2.Name = "BrowseLBL2";
+            this.BrowseLBL2.Size = new System.Drawing.Size(0, 15);
+            this.BrowseLBL2.TabIndex = 5;
+            // 
+            // SiftButton
+            // 
+            this.SiftButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.SiftButton.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SiftButton.Location = new System.Drawing.Point(228, 265);
+            this.SiftButton.Name = "SiftButton";
+            this.SiftButton.Size = new System.Drawing.Size(66, 23);
+            this.SiftButton.TabIndex = 6;
+            this.SiftButton.Text = "Sift!";
+            this.SiftButton.UseVisualStyleBackColor = false;
+            this.SiftButton.Click += new System.EventHandler(this.SiftButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // SheetNameTB
+            // 
+            this.SheetNameTB.Location = new System.Drawing.Point(174, 188);
+            this.SheetNameTB.Name = "SheetNameTB";
+            this.SheetNameTB.Size = new System.Drawing.Size(87, 23);
+            this.SheetNameTB.TabIndex = 7;
+            this.SheetNameTB.Text = "Sheet1";
+            this.SheetNameTB.TextChanged += new System.EventHandler(this.SheetNameTB_TextChanged);
+            // 
+            // SheetNameLBL
+            // 
+            this.SheetNameLBL.AutoSize = true;
+            this.SheetNameLBL.Location = new System.Drawing.Point(121, 161);
+            this.SheetNameLBL.Name = "SheetNameLBL";
+            this.SheetNameLBL.Size = new System.Drawing.Size(202, 15);
+            this.SheetNameLBL.TabIndex = 8;
+            this.SheetNameLBL.Text = "Enter name of sheet you want to sift";
+            this.SheetNameLBL.Click += new System.EventHandler(this.SheetNameLBL_Click);
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.RestartButton.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RestartButton.Location = new System.Drawing.Point(145, 265);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(66, 23);
+            this.RestartButton.TabIndex = 9;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = false;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // hasHeaderRow
+            // 
+            this.hasHeaderRow.AutoSize = true;
+            this.hasHeaderRow.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.hasHeaderRow.Location = new System.Drawing.Point(161, 230);
+            this.hasHeaderRow.Name = "hasHeaderRow";
+            this.hasHeaderRow.Size = new System.Drawing.Size(121, 19);
+            this.hasHeaderRow.TabIndex = 10;
+            this.hasHeaderRow.Text = "Has Header Row?";
+            this.hasHeaderRow.UseVisualStyleBackColor = false;
+            // 
+            // ExcelDuplicateSifter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(430, 359);
+            this.Controls.Add(this.hasHeaderRow);
+            this.Controls.Add(this.RestartButton);
+            this.Controls.Add(this.SheetNameLBL);
+            this.Controls.Add(this.SheetNameTB);
+            this.Controls.Add(this.SiftButton);
+            this.Controls.Add(this.BrowseLBL2);
+            this.Controls.Add(this.BrowseLBL1);
+            this.Controls.Add(this.BrowseTB1);
+            this.Controls.Add(this.Browse1);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ExcelDuplicateSifter";
+            this.Text = "Excel Duplicate Sifter";
+            this.Load += new System.EventHandler(this.ExcelDuplicateSifter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse1;
+        private System.Windows.Forms.TextBox BrowseTB1;
+        private System.Windows.Forms.Label BrowseLBL1;
+        private System.Windows.Forms.Label BrowseLBL2;
+        private System.Windows.Forms.Button SiftButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox SheetNameTB;
+        private System.Windows.Forms.Label SheetNameLBL;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.CheckBox hasHeaderRow;
+    }
+}
+
---
+++ FilterExcelDuplicates/FilterExcelDuplicates/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace FilterExcelDuplicates
+{
+    public partial class ExcelDuplicateSifter : Form
+    {
+        public ExcelDuplicateSifter()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse1_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = openFileDialog1.ShowDialog(this); // Show the dialog.
+            
+            openFileDialog1.DefaultExt = ".xlsx";
+
+            string ext = Path.GetExtension(openFileDialog1.FileName);
+
+            if (result == DialogResult.OK && (ext.Equals(".xlsx") || ext.Equals(".xls") || ext.Equals(".csv"))) // Test result.
+            {
+                BrowseTB1.Text = openFileDialog1.FileName;
+               
+                SiftButton.Enabled = true;
+                SheetNameTB.Enabled = true;
+                SheetNameLBL.Enabled = true;
+                hasHeaderRow.Enabled = true;
+                SheetNameTB.Text = "Sheet1";
+
+            }
+            else
+            {
+                MessageBox.Show("Incorrect File Type");
+            }
+        }
+
+        private void ExcelDuplicateSifter_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+            SiftButton.Enabled = false;
+            BrowseTB1.Enabled = false;
+            SheetNameTB.Enabled = false;
+            SheetNameLBL.Enabled = false;
+            hasHeaderRow.Enabled = false;
+        }
+
+        private void BrowseTB1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SiftButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filenm = BrowseTB1.Text;
+                if (SheetNameTB.Text == "")
+                {
+                    MessageBox.Show("Please Enter a Sheet name.");
+                    return;
+                }
+                //This code re-generates the entire Building/Room for all of campus (as listed in the main inventory file)
+                //TODO: change path to generic OneDrive Path for inventory, BasicConfig, and Access Control, revise and rename sheet as well.
+                //TODO: Add xml Wrappers <root> or whatever. This is a complete generation.
+                NPOI.XSSF.UserModel.XSSFWorkbook xssfwb;
+                using (FileStream file = new FileStream(filenm, FileMode.Open, FileAccess.Read))
+                {
+                    xssfwb = new NPOI.XSSF.UserModel.XSSFWorkbook(file);
+                }
+                ISheet sheet = xssfwb.GetSheet(SheetNameTB.Text);
+                int numRows = sheet.LastRowNum;
+                int numCols = sheet.GetRow(0).LastCellNum;
+
+                List<string> values = new List<string>();
+
+                try
+                {
+                    using (StreamWriter configFile = File.AppendText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\SiftedResults.txt"))
+                    {
+                        for (int row = hasHeaderRow.Checked ? 1 : 0; row <= numRows; row++) //That's dope
+                        {
+                            for (int col = 0; col < numCols; col++)
+                            {
+                                string cellVal = sheet.GetRow(row).GetCell(col).StringCellValue;
+
+                                if (!values.Contains(cellVal))
+                                {
+                                    values.Add(cellVal);
+                                }
+                            }
+                        }
+
+                        foreach (string val in values)
+                        {
+                            configFile.WriteLine(val);
+                        }
+
+                        MessageBox.Show("The Results have been saved to your Desktop.");
+
+                        RestartButton_Click(sender, e);
+
+                    }
+                }
+                catch (NullReferenceException) //Hey man, it just happens sometimes.
+                {
+                    //Do nothing
+                }
+            } catch (Exception er)
+            {
+               MessageBox.Show("Error: " + er);
+            }
+
+
+        }
+
+        private void SheetNameTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SheetNameLBL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            BrowseTB1.Text = "";
+            SheetNameTB.Text = "";
+            SiftButton.Enabled = false;
+            BrowseTB1.Enabled = false;
+            SheetNameTB.Enabled = false;
+            SheetNameLBL.Enabled = false;
+            hasHeaderRow.Enabled = false;
+        }
+    }
+}
